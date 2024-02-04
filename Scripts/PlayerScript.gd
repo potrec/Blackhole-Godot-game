078551de7e78bbeb1b732f1647c764c3b07ab035
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-02-04 20:03:50 +0100

    Add animation to the black hole

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -10,7 +10,7 @@ func _ready():
 	weight = get_meta("Weight")
 	scale.x = size
 	scale.y = size
-
+	$AnimationPlayer.current_animation = "Test"
 func _process(delta):
 	var direction = Input.get_vector("Right","Left","Down","Up")
 	position -= speed*delta*direction
