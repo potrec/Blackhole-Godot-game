5f4e90386c8fe3f903cf52bacb629ebbf883178a
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-02-05 18:04:41 +0100

    Work on zoom when blackhole eat food

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
-@export var speed: float = 0
-@export var size: float = 0
-@export var weight: float = 0
+@export var speed: float
+@export var size: float
+@export var weight: float
 var objects = []
-@onready var defaultCameraZoom = $Camera2D.zoom
 const objectRelativeMinSpeed = 1.25
 @export var level: Node2D
+@onready var currentZoom: float = $Camera2D.zoom.x
+@onready var targetZoom: float = $Camera2D.zoom.x
+@export var zoomSpeed: float = 10.0
+@onready var ratio = currentZoom / scale.x
 
 func _ready():
 	scale = Vector2(size, size)
@@ -24,8 +27,19 @@ func _process(delta):
 		if position.distance_to(object.position) < 1:
 			level.remove_food(object)
 			scale += Vector2(0.3,0.3)
-			$Camera2D.zoom = defaultCameraZoom / scale 
+			targetZoom = targetZoom / scale.x
+			ratio = targetZoom / scale.x
 			objects = objects.filter(func(e): return e != object)
+	if abs(currentZoom - targetZoom) > 0.1:
+		camera_zoom(delta)
+	print(scale," ",targetZoom," ",ratio)
+	
 
 func _on_body_entered(body):
 	objects.push_back(body)
+	
+func camera_zoom(delta):
+	currentZoom = lerp(currentZoom, targetZoom, zoomSpeed * delta)
+	$Camera2D.zoom = Vector2(currentZoom, currentZoom)
+
+
