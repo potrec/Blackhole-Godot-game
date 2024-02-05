b62d9fdbc9417addea9d461258f6637c8c919a32
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-02-06 00:59:41 +0100

    Fix zoom

--- Scenes/LevelScene.gd
+++ Scenes/LevelScene.gd
@@ -10,14 +10,6 @@ func _ready():
 		create_food()
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-func _physics_process(delta):
-	pass
-
 func remove_food(object):
 	object.queue_free()
 	create_food()
--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -7,12 +7,11 @@ var objects = []
 const objectRelativeMinSpeed = 1.25
 @export var level: Node2D
 @onready var currentZoom: float = $Camera2D.zoom.x
+@onready var defaultZoom: float = $Camera2D.zoom.x
 @onready var targetZoom: float = $Camera2D.zoom.x
 @export var zoomSpeed: float = 10.0
-@onready var ratio = currentZoom / scale.x
 
 func _ready():
-	scale = Vector2(size, size)
 	$AnimationPlayer.current_animation = "Test"
 	
 func _process(delta):
@@ -26,15 +25,12 @@ func _process(delta):
 		object.position += objectSpeed
 		if position.distance_to(object.position) < 1:
 			level.remove_food(object)
-			scale += Vector2(0.3,0.3)
-			targetZoom = targetZoom / scale.x
-			ratio = targetZoom / scale.x
+			scale += Vector2(1,1)
+			targetZoom = (defaultZoom / scale.x)
 			objects = objects.filter(func(e): return e != object)
-	if abs(currentZoom - targetZoom) > 0.1:
+	if abs(currentZoom - targetZoom) > 0.01:
 		camera_zoom(delta)
-	print(scale," ",targetZoom," ",ratio)
-	
-
+		
 func _on_body_entered(body):
 	objects.push_back(body)
 	
