a4b5476204ca3de04450120d89ce32d0b43ae8f4
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-02-05 02:40:59 +0100

    Add pulling objects to the black hole

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends Area2D
 
 var speed: float = 0
 var size: float = 0
 var weight: float = 0
-
+var objects = []
+const objectRelativeMinSpeed = 1.25
 func _ready():
 	speed = get_meta("Speed")
 	size = get_meta("Size")
@@ -12,5 +13,17 @@ func _ready():
 	scale.y = size
 	$AnimationPlayer.current_animation = "Test"
 func _process(delta):
-	var direction = Input.get_vector("Right","Left","Down","Up")
-	position -= speed*delta*direction
+	var direction = Input.get_vector("Left","Right","Up","Down")
+	var transport = speed*delta*direction
+	position += transport
+	for object in objects:
+		var objectSpeed = (position - object.position).normalized()/position.distance_to(object.position)*delta*speed*$CollisionShape2D.shape.radius*scale.x
+		if objectSpeed.length() < transport.length()*objectRelativeMinSpeed:
+			objectSpeed = objectSpeed.normalized()*transport.length()*objectRelativeMinSpeed
+		object.position += objectSpeed
+		if position.distance_to(object.position) < 1:
+			object.queue_free()
+			objects = objects.filter(func(e): return e != object)
+
+func _on_body_entered(body):
+	objects.push_back(body)
